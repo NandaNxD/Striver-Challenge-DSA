e85864f53d28d142debd8d4997f4bce7f6305684
NandaNxD <nanven9@gmail.com>
2022-08-09 17:54:08 +0530

    First Commit

---
+++ A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int main(){
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    ll t;cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        cout<<n<<endl;
+    }
+}

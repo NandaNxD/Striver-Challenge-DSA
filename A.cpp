b13180df62c34e27be42001cb13fa412544d89fa
NandaNxD <nanven9@gmail.com>
2022-08-16 12:39:32 +0530

    MergeTwoSortedLinkedLists

--- A.cpp
+++ A.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include<stack>
 using namespace std;
 typedef long long ll;
 
@@ -6,6 +7,7 @@ typedef long long ll;
 int main(){
     freopen("input","r",stdin);
     freopen("output","w",stdout);
+    stack<int> s;
     ll t;cin>>t;
     while(t--){
         ll n;
